3e13a800ef33f53b79ac66be2aefa2fa6529d13f
Sam Day <sam.c.day@gmail.com>
2012-10-06 10:34:34 +1000

    logback.groovy to turn the noise waaaay the fuck down.

---
+++ src/test/resources/logback.groovy
@@ -0,0 +1,11 @@
+import static ch.qos.logback.classic.Level.*
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+
+appender("STDOUT", ConsoleAppender) {
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
+    }
+}
+
+root(WARN, ["STDOUT"])
