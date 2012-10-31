d074daec35e035bca26e2e52ab7c8f31adf64083
Sam Day <sam.c.day@gmail.com>
2012-10-31 21:35:23 +1000

    More tests for LayoutRepository

--- src/test/java/com/site2go/dao/repositories/jpa/LayoutRepositoryIT.java
+++ src/test/java/com/site2go/dao/repositories/jpa/LayoutRepositoryIT.java
@@ -4,6 +4,7 @@ import com.site2go.dao.entities.LayoutEntity;
 import com.site2go.dao.repositories.LayoutRepository;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.util.Set;
 
@@ -27,5 +28,17 @@ public class LayoutRepositoryIT extends RepositoryITBase {
         Set<LayoutEntity> layouts = this.layoutRepository.listBySite(123);
         assertThat(layouts, notNullValue());
         assertThat(layouts.size(), is(0));
+    }
+
+    @Test
+    public void testFindBySiteAndSlug() {
+        LayoutEntity layout = this.layoutRepository.findBySiteAndSlug(1, "testlayout");
+        assertThat(layout.getId(), is(1));
+        assertThat(layout.getSite().getId(), is(1));
+    }
+
+    @Test(expected = EmptyResultDataAccessException.class)
+    public void testFindByNonexistentSiteAndSlug() {
+        LayoutEntity layout = this.layoutRepository.findBySiteAndSlug(123, "foo");
     }
 }
